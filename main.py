51e82415315b6b9ba160f8b1f832b8afa83ca329
Navjot Gill <105019681+ng19011@users.noreply.github.com>
2022-06-02 21:01:58 +0000

    i added a print statement greeting and welcoming the user to the game

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from random import randint
-player = input ('rock (r), paper (p) or scissors (s)?')
+print("hello and welcome to the game rock, paper and sissors")
+player = input('rock (r), paper (p) or scissors (s)?')
 print(player, 'vs', end='')
-chosen = randint(1,3)
+chosen = randint(1, 3)
 #print(chosen)
 print(chosen)
 if chosen == 1:
-  computer = 'r'
+    computer = 'r'
 
 elif chosen == 2:
-  computer = 'p'
+    computer = 'p'
 
 else:
-  computer = 's'
+    computer = 's'
 print(computer)
 
 if player == computer:
-  print('DRAW!')
+    print('DRAW!')
 elif player == 'r' and computer == 's':
-  print('Player wins!')
+    print('Player wins!')
 
 elif player == 'r' and computer == 'p':
-  print('Computer wins!')
+    print('Computer wins!')
 
 elif player == 'p' and computer == 'r':
-  print('Player wins!')
+    print('Player wins!')
 
 elif player == 'p' and computer == 's':
-  print('Computer wins!')
-
+    print('Computer wins!')
